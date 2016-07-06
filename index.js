7756eb5ce02840b0ae2d285df8420d9f70e8dcfe
Brian Woodward <brian.woodward@gmail.com>
2016-07-06 16:44:12 -0400

    implementation

--- index.js
+++ index.js
@@ -1,4 +1,108 @@
+/*!
+ * detect-file (https://github.com/doowb/detect-file)
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
 'use strict';
 
-module.exports = function() {
+var fs = require('fs');
+var path = require('path');
+var exists = require('fs-exists-sync');
+
+/**
+ * Resolve the given `filepath` if it exists.
+ *
+ * ```js
+ * var res = detect('package.json');
+ * console.log(res);
+ * //=> "package.json"
+ *
+ * var res = detect('fake-file.json');
+ * console.log(res)
+ * //=> null
+ * ```
+ *
+ * @name detect
+ * @param  {String} `filepath` filepath to detect.
+ * @return {String} Returns the resolved filepath if it exists, otherwise returns `null`.
+ * @api public
+ */
+
+module.exports = function(filepath) {
+  if (!filepath || (typeof filepath !== 'string')) {
+    return null;
+  }
+  if (exists(filepath)) {
+    return path.resolve(filepath);
+  }
+
+  if (process.platform === 'win32') {
+    return null;
+  }
+  return detect(filepath);
 };
+
+/**
+ * Check if the filepath exists by falling back to reading in the entire directory.
+ * Returns the real filepath (for case sensitive file systems) if found.
+ *
+ * @param  {String} `filepath` filepath to check.
+ * @return {String} Returns found filepath if exists, otherwise null.
+ */
+
+function detect(filepath) {
+  filepath = path.resolve(filepath);
+  var res = tryReaddir(filepath);
+  if (res === null) {
+    return null;
+  }
+
+  // "filepath" is a directory, an error would be
+  // thrown if it doesn't exist. if we're here, it exists
+  if (res.path === filepath) {
+    return res.path;
+  }
+
+  // "filepath" is not a directory
+  // compare against upper case later
+  // see https://nodejs.org/en/docs/guides/working-with-different-filesystems/
+  var upper = filepath.toUpperCase();
+  var len = res.files.length;
+  var idx = -1;
+
+  while (++idx < len) {
+    var fp = path.resolve(res.path, res.files[idx]);
+    if (filepath === fp || upper === fp) {
+      return fp;
+    }
+    var fpUpper = fp.toUpperCase();
+    if (filepath === fpUpper || upper === fpUpper) {
+      return fp;
+    }
+  }
+
+  return null;
+}
+
+/**
+ * Try to read the filepath as a directory first, then fallback to the filepath's dirname.
+ *
+ * @param  {String} `filepath` path of the directory to read.
+ * @return {Object} Object containing `path` and `files` if succesful. Otherwise, null.
+ */
+
+function tryReaddir(filepath) {
+  var ctx = { path: filepath, files: [] };
+  try {
+    ctx.files = fs.readdirSync(filepath);
+    return ctx;
+  } catch (err) {}
+  try {
+    ctx.path = path.dirname(filepath);
+    ctx.files = fs.readdirSync(ctx.path);
+    return ctx;
+  } catch (err) {}
+  return null;
+}
