768a6ffb4c9957e2d6b842ef959c0e5a8d00182b
doowb <brian.woodward@gmail.com>
2017-08-05 23:03:25 -0400

    remove fuzzy-match specific test and documentation

--- test.js
+++ test.js
@@ -37,8 +37,8 @@ describe('detect', function() {
   });
 
   it('should handle case sensitive full filepaths on linux', function() {
-    var fp = path.resolve('fixtures/a/b/abc/def/mixedcasefile.txt');
-    assert.equal(detect(fp, {nocase: true}), path.resolve(isLinux ? 'fixtures/a/B/aBc/DeF/MixedCaseFile.txt' : 'fixtures/a/b/abc/def/mixedcasefile.txt'));
+    var fp = path.resolve('fixtures/a/B/aBc/DeF/mixedcasefile.txt');
+    assert.equal(detect(fp, {nocase: true}), path.resolve(isLinux ? 'fixtures/a/B/aBc/DeF/MixedCaseFile.txt' : 'fixtures/a/B/aBc/DeF/mixedcasefile.txt'));
 
     if (isLinux) {
       assert(!detect(fp), 'expected null when not using the sensitive flag on linux');
