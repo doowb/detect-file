7b0bef3ac292161eab62e777b12e4e0d538cc59b
Edward Betts <edward@4angle.com>
2017-09-01 19:50:38 +0100

    correct spelling mistake

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function nocase(filepath) {
  * Try to read the filepath as a directory first, then fallback to the filepath's dirname.
  *
  * @param  {String} `filepath` path of the directory to read.
- * @return {Object} Object containing `path` and `files` if succesful. Otherwise, null.
+ * @return {Object} Object containing `path` and `files` if successful. Otherwise, null.
  */
 
 function tryReaddir(filepath) {
